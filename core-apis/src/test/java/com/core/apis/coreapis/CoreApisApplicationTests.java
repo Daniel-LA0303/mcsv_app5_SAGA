c1a708fce764b479a15455ede95e6064d34b37af
Daniel-LA0303 <03daniel030399@gmail.com>
2023-09-28 12:30:43 -0600

    first commit

---
+++ core-apis/src/test/java/com/core/apis/coreapis/CoreApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.apis.coreapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
